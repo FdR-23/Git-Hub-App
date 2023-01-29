258397f47bbdf31da3743827bfed46a9b20c6505
FeDe <federampi@gmail.com>
2023-01-29 18:37:27 -0300

    fix env

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,12 +7,12 @@ import './index.css'
 import UserProvider from './components/UserProvider.jsx'
 //import Pagination from './services/PaginationProvider.jsx'
 
-axios.defaults.baseURL = "http://localhost:3001"
+axios.defaults.baseURL = import.meta.env.VITE_DEPLOY_APY||"http://localhost:3001"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
   <UserProvider>
-      <App />
+    <App />
   </UserProvider>
   // </React.StrictMode>,
 )
